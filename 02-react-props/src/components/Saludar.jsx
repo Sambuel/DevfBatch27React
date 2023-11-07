8a099831ab12593f8ca03343fbb8ed2390c1520f
Sambuel <123chamollito@gmail.com>
2023-11-06 18:55:01 -0600

    proyecto estructurado

---
+++ 02-react-props/src/components/Saludar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Saludar = ({nombre,apellido}) => {
+
+
+ return (
+    <div>
+        <h1>hola {nombre}{apellido}</h1>
+    </div>
+  )
+}
+
+export default Saludar
+
+   
