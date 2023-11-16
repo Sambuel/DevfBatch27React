cdba7b45f562b4b6a7e82c17b370a3eee6bfa010
Sambuel <123chamollito@gmail.com>
2023-11-16 12:06:42 -0600

    ToDoList Finalizado

---
+++ 03-todo-list/src/components/ToDoItem.jsx
@@ -0,0 +1,11 @@
+
+const ToDoItem = ({todo , handleDelete}) => {
+  return (
+    <li>
+        {todo}
+        <button onClick={handleDelete} className="delete-button">Eliminar</button>
+  </li>
+  )
+} 
+
+export default ToDoItem
--- 03-todo-list/src/components/ToDolist.jsx
+++ 03-todo-list/src/components/ToDolist.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react"
+import ToDoItem from "./ToDoItem";
 
 const ToDolist = () => {
 const [inputValue, setInputValue] = useState("")
+const [tareas,setTareas] = useState([])
+
+const handleAdd = () => {
+  if (inputValue.trim()) {
+    setTareas([...tareas, inputValue]); // Corregir aquí
+    setInputValue("");
+  }
+};
 
 
+const handleDelete = (indiceTarea) => {
+setTareas(tareas.filter((tarea,indice)=> indice !== indiceTarea))
+}
+
+// console.log([1,2,3,4].filter((numero)=>numero <3));  -->    // el metodo filter tiene una condicion por ejemplo este console.log solo imprimio los numero menores a 3 poniendolos a en un nuevo arreglo 
+
+// console.log([1,2,3,4].map((numero)=>numero + 1));
 
   return (
     <div>
@@ -13,6 +29,17 @@ const [inputValue, setInputValue] = useState("")
         value={inputValue}
         onChange={(evento)=> setInputValue(evento.target.value)}  
         />
+        <button 
+        className="add-button"
+        onClick={handleAdd}
+        >
+          Agregar
+            </button>
+        <ul>
+        {tareas.map((tarea,indice)=>(
+          <ToDoItem todo={tarea}handleDelete={(e) => handleDelete(indice)} key={indice}/>
+        ))}
+        </ul>          
     </div>
   )
 }
