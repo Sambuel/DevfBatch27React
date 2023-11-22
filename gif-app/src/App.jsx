70a7a14cafa056af6d3d2646dc7a2b7c1563678b
Sambuel <123chamollito@gmail.com>
2023-11-22 00:24:55 -0600

    gif-app finalizado

--- gif-app/src/App.jsx
+++ gif-app/src/App.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import ImageCard from './components/ImageCard'
 import SearchBar from './components/SearchBar'
+
 function App () {
   const [gifs, setGifs] = useState([])
   const APIKEY = import.meta.env.VITE_GIPHY_API_KEY
+
   const sendSearch = (search) => {
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${search}&limit=30&offset=0&rating=g&lang=en`)
     .then(res => res.json())
@@ -14,6 +16,8 @@ function App () {
       setGifs(data)
     }).catch(err => console.log(err))
   }
+
+  
   // Las llamadas a API ocurren normalmente en el useEffect.
   // useEffect usa un arreglo de dependencias vacio para que solo se ejecute una vez.
   useEffect(() => {
